6d0204e16ffa2cd34177b5721be979c383f011b8
Zeev17 <papini.antoine@gmail.com>
2022-02-07 15:33:06 +0100

    correction de la classe Livre et de l'application Bibliotheque

--- Livre/Bibliotheque.java
+++ Livre/Bibliotheque.java
@@ -4,30 +4,35 @@ public class Bibliotheque {
 	public static void main(String [] arg) {
 		
 		Scanner lectureClavier = new Scanner(System.in);
+
+		Livre unPolar = new Livre("Le mystère de la chambre jaune","Leoux","Gaston","Policier","2253005495");
+
+		unPolar.afficherUnLivre();
+
+
+		// 8.5.b Appel du constructeur par défaut	
+		// Livre livrePoche = new Livre();
+
 		
-		// 7.3.b Défintion de l'objet livrePoche
-		Livre livrePoche = new Livre();
+		// System.out.println("Entrez le tire : ");
+		//livrePoche.setTitre(lectureClavier.next());
 		
-		// 7.3.b saisie du titre de l'ouvrage
-		System.out.println("Entrez le tire : ");
-		livrePoche.titre = lectureClavier.next();
-		
-		// 7.3.b saisie du nom de l'auteur de l'ouvrage
-		System.out.println("Entrez le nom de l'auteur : ");
-		livrePoche.nomAuteur = lectureClavier.next();
-		
-		// 7.3.b saisie du prénom de l'auteur de l'ouvrage
-		System.out.println("Entrez le prénom de l'auteur : ");
-		livrePoche.prenomauteur = lectureClavier.next();
-		
-		// 7.3.b saisie de l'ISBN de l'ouvrage
-		System.out.println("Entrez le numéro d'ISBN : ");
-		livrePoche.isbn = lectureClavier.next();
+		// System.out.println("Entrez la catégorie : ");
+		// livrePoche.setCategorie(lectureClavier.next());
+		// 
+		// System.out.println("Entrez le nom de l'auteur : ");
+		// livrePoche.setNomAuteur(lectureClavier.next());
+		// 
+		// System.out.println("Entrez le prénom de l'auteur : ");
+		// livrePoche.setPrenomAuteur(lectureClavier.next());
+		// 
+		// System.out.println("Entrez le numéro d'ISBN : ");
+		// livrePoche.setIsbn(lectureClavier.next());
 		
 		// 7.4.b Affichage des caractéristiques de l'objet livrePoche
-		livrePoche.afficherUnLivre();
+		//livrePoche.afficherUnLivre();
 		
 		// 7.4.d Affichage du code de l'objet livrePoche
-		System.out.println("code du livre : " + livrePoche.calculerLeCode());
+		// System.out.println("code du livre : " + livrePoche.calculerLeCode());
 	}	
 }
--- Livre/Livre.java
+++ Livre/Livre.java
@@ -6,19 +6,21 @@ public class Livre {
 	public String categorie;
 	public String isbn;
 	public String nomAuteur;
-	public String prenomauteur;
+	public String prenomAuteur;
 	//public string code;
 
 	// 7.4.a Définition des comportements : La méthode afficherUnLivre()
 	public void afficherUnLivre() {
 		System.out.println("Titre : " + titre);
-		System.out.println("Auteur : " + nomAuteur + " " + prenomauteur);
+		System.out.println("Auteur : " + nomAuteur + " " + prenomAuteur);
 		System.out.println("Categorie : " + categorie);
 		System.out.println("ISBN : " + isbn);
+		// 8.4 Appel de la méthode invisible
+		System.out.println("Code : " + getCode());
 	}
 
 	// 7.4.C Définition des comportements : La méthode calculerLeCode()
-	public String calculerLeCode(){
+	private String getCode(){
 		String debutNom;
 		String debutPrenom;
 		String debutCategorie;
@@ -27,7 +29,7 @@ public class Livre {
 		// 7.4.c Récupérer les deux premières lettres du nom
 		debutNom = nomAuteur.substring(0,2);
 		// 7.4.c Récupérer les deux premières lettres du prénom
-		debutPrenom = prenomauteur.substring(0,2);
+		debutPrenom = prenomAuteur.substring(0,2);
 		// 7.4.c Récupérer les deux premières lettres de la catégorie
 		debutCategorie = categorie.substring(0,2);
 		// 7.4.c Récupérer les deux dernières lettres de l'ISBN
@@ -35,6 +37,73 @@ public class Livre {
 		finIsbn = isbn.substring((longueurIsbn-2), longueurIsbn);
 		// 7.4.c Retourner la suite des caractères extraits ci-avant
 		return debutNom+debutPrenom+debutCategorie+finIsbn;
+	}
+
+	// 8.2.a Définition des méthodes d'accès en écriture
+	// Le Titre
+	public void setTitre (String valeur) {
+		titre = valeur;
+	}
+	// La catégorie
+	public void setCategorie(String valeur) {
+		categorie = valeur;
+	}
+	// Nuemro ISBN
+	public void setIsbn (String valeur) {
+		isbn = valeur;
+	}
+	// Nom de l'auteur
+	public void setNomAuteur (String valeur) {
+		nomAuteur = valeur;
+	}
+	// Prénom de l'auteur
+	public void setPrenomAuteur (String valeur) {
+		prenomAuteur = valeur;
+	}
+	
+	// 8.3.a Définition des méthodes d'accès en lecture
+	// Le titre
+	public String getTitre() {
+		return titre;
+	}
+	// Categorie
+	public String getCategorie() {
+		return categorie;
+	}
+	// Numero Isbn
+	public String getIsbn() {
+		return isbn;
+	}
+	// Nom de l'auteur
+	public String getNomAuteur() {
+		return nomAuteur;
+	}
+	// Prénom de l'auteur
+	public String getPrenomAuteur() {
+		return prenomAuteur;
+	}
+
+	// 8.5.a Défintion du constructeur par défaut
+	public Livre() {
+		Scanner lectureClavier = new Scanner(System.in);
+		System.out.print("Entrez le titre : ");
+		setTitre(lectureClavier.next());
+		System.out.print("Entrez la categorie : ");
+		setCategorie(lectureClavier.next());
+		System.out.print("Entrez le nom de l'auteur : ");
+		setNomAuteur(lectureClavier.next());
+		System.out.print("Entrez le prénom de l'auteur : ");
+		setPrenomAuteur(lectureClavier.next());
+		System.out.print("Entrez l'Isbn : ");
+		setIsbn(lectureClavier.next());
+	}
+
+	public Livre(String t, String c, String na, String pa, String i) {
+		setTitre(t);
+		setCategorie(c);
+		setNomAuteur(na);
+		setPrenomAuteur(pa);
+		setIsbn(i);
 	}
 
 }
